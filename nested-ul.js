5de878ee62399f8135d2c3e20cea614640627288
Ahmed El-Telbani <69460945+ahmedtelbani@users.noreply.github.com>
2024-10-31 15:24:04 +0300

    Update nested-ul.js

--- nested-ul.js
+++ nested-ul.js
@@ -3,69 +3,47 @@
 * if the number of sub-items is more than 5, it will be in big-sub-menu class
 */
 document.addEventListener("DOMContentLoaded", function() {
-    var linkList = document.getElementById("LinkList3");
-    
-    if (linkList) {
-        var e = "<ul id='nav'><li><ul id='sub-menu'>";
-        var items = linkList.getElementsByTagName("li");
-        
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            var t = item.textContent; // Get the text of the current list item
-            var n = t.substr(0, 1);    // Get the first character
-            var r = t.substr(1);       // Get the rest of the text
-            var link = item.querySelector("a"); // Get the anchor element
+    const linkList3 = document.getElementById("LinkList3");
 
-            if (n === "_") {
-                // If the first character is an underscore
-                if (link) {
-                    n = link.getAttribute("href"); // Get the href attribute
-                    e += '<li><a href="' + n + '">' + r + "</a></li>"; // Add to the sub-menu
-                }
+    if (linkList3) {
+        let navHTML = "<ul id='nav'><li><ul class='sub-menu'>";
+
+        // Iterate through each list item
+        Array.from(linkList3.getElementsByTagName("li")).forEach((li) => {
+            const text = li.textContent.trim();
+            const prefix = text.charAt(0);
+            const content = text.slice(1);
+            const href = li.querySelector("a").getAttribute("href");
+
+            if (prefix === "_") {
+                // Add to sub-menu
+                navHTML += `<li><a href="${href}">${content}</a></li>`;
             } else {
-                // Otherwise, create a new menu item
-                if (link) {
-                    n = link.getAttribute("href"); // Get the href attribute
-                    e += '</ul></li><li><a href="' + n + '">' + t + "</a><ul id='sub-menu'>"; // Open new sub-menu
-                }
-            }
-        }
-
-        e += "</ul></li></ul>"; // Close the menu structure
-        linkList.innerHTML = e; // Set the generated HTML as the content of #LinkList3
-
-        // Remove empty sub-menus
-        var subMenus = linkList.getElementsByTagName("ul");
-        for (var j = subMenus.length - 1; j >= 0; j--) {
-            var subMenu = subMenus[j];
-            if (subMenu.innerHTML.replace(/\s|&nbsp;/g, "").length === 0) {
-                subMenu.remove();
-            }
-        }
-
-        // Check sub-menu lengths and update classes
-        var allSubMenus = linkList.querySelectorAll("ul#sub-menu");
-        allSubMenus.forEach(function(subMenu) {
-            var itemsInSubMenu = subMenu.getElementsByTagName("li");
-            if (itemsInSubMenu.length > 5) {
-                subMenu.id = 'big-sub-menu'; // Change the sub-menu ID to big-sub-menu
-                subMenu.classList.add('big-sub-menu'); // Add the big-sub-menu class
+                // Close previous sub-menu and open a new one
+                navHTML += `</ul></li><li><a href="${href}">${text}</a><ul class='sub-menu'>`;
             }
         });
 
-        // Remove empty list items
-        var listItems = linkList.getElementsByTagName("li");
-        for (var k = listItems.length - 1; k >= 0; k--) {
-            var listItem = listItems[k];
-            if (listItem.innerHTML.replace(/\s|&nbsp;/g, "").length === 0) {
-                listItem.remove();
-            }
-        }
+        navHTML += "</ul></li></ul>";
+        linkList3.innerHTML = navHTML;
 
-        // Add 'has-sub' class to links that have a sub-menu
-        var parentLinks = linkList.querySelectorAll('ul#sub-menu').parentNode.querySelectorAll('a');
-        parentLinks.forEach(function(link) {
-            link.classList.add('has-sub');
+        // Remove empty sub-menus and items
+        linkList3.querySelectorAll("ul").forEach((ul) => {
+            if (ul.textContent.trim() === "") ul.remove();
+        });
+        linkList3.querySelectorAll("li").forEach((li) => {
+            if (li.textContent.trim() === "") li.remove();
+        });
+
+        // Add 'has-sub' class to parent links
+        linkList3.querySelectorAll("ul.sub-menu").forEach((subMenu) => {
+            const parentLink = subMenu.parentElement.querySelector("a");
+            if (parentLink) parentLink.classList.add("has-sub");
+
+            // Change class to 'big-sub-menu' if sub-menu has more than 5 items
+            if (subMenu.children.length > 5) {
+                subMenu.classList.replace("sub-menu", "big-sub-menu");
+            }
         });
     }
 });
